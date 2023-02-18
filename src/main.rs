984716db5bf7f4d81ee3fa398229f54640e7a9bc
Jumpei Mori <nikutto.kom@gmail.com>
2023-02-19 08:56:14 +0900

    Add hello world api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Serialize;
+use actix_web::{App, HttpServer, Responder, HttpResponse};
+use actix_web::web::get;
+
+#[derive(Serialize)]
+struct SimpleMessage {
+    message: String,
+}
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().json(
+        SimpleMessage { message: "hello world".to_string() }
+    )
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            .route("/api/hello", get().to(hello))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }

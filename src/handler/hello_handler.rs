9d64f8b551fa3635cc2eb49d079e9023b8926191
Jumpei Mori <nikutto.kom@gmail.com>
2023-02-19 10:17:25 +0900

    Fix structure

--- src/handler/hello_handler.rs
+++ src/handler/hello_handler.rs
@@ -1,6 +1,6 @@
 use crate::service::hello_service;
 
-use actix_web::{get, post, HttpResponse, Responder};
+use actix_web::{get, post, HttpResponse, Responder, web::ServiceConfig};
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -9,13 +9,18 @@ struct SimpleMessage {
 }
 
 #[get("/api/v1/hello")]
-pub async fn hello() -> impl Responder {
+async fn hello() -> impl Responder {
     let msg = hello_service::get_hello_msg();
     HttpResponse::Ok().json(SimpleMessage { message: msg })
 }
 
 #[post("/api/v1/hello/log-check/info")]
-pub async fn log_check_info() -> impl Responder {
+async fn log_check_info() -> impl Responder {
     hello_service::gen_info_log();
     HttpResponse::NoContent()
+}
+
+pub fn configure(cfg: &mut ServiceConfig) {
+    cfg.service(hello);
+    cfg.service(log_check_info);
 }

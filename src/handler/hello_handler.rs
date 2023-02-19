c94a2d8180450d28048f35a46ab045a4445ab50d
Jumpei Mori <nikutto.kom@gmail.com>
2023-02-19 10:15:34 +0900

    Add info log test handler

--- src/handler/hello_handler.rs
+++ src/handler/hello_handler.rs
@@ -1,6 +1,6 @@
 use crate::service::hello_service;
 
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, post, HttpResponse, Responder};
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -9,7 +9,13 @@ struct SimpleMessage {
 }
 
 #[get("/api/v1/hello")]
-async fn hello() -> impl Responder {
+pub async fn hello() -> impl Responder {
     let msg = hello_service::get_hello_msg();
     HttpResponse::Ok().json(SimpleMessage { message: msg })
+}
+
+#[post("/api/v1/hello/log-check/info")]
+pub async fn log_check_info() -> impl Responder {
+    hello_service::gen_info_log();
+    HttpResponse::NoContent()
 }
--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -2,5 +2,7 @@ mod hello_handler;
 use actix_web::web::ServiceConfig;
 
 pub fn configure(cfg: &mut ServiceConfig) {
-    cfg.service(hello_handler::hello);
+    cfg
+        .service(hello_handler::hello)
+        .service(hello_handler::log_check_info);
 }

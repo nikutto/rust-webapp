f0e0ff991df3aa8d11e021bb12a442bde3068e96
Jumpei Mori <nikutto.kom@gmail.com>
2023-02-24 08:55:41 +0900

    Enable prometheus for rust app

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,25 @@ mod repository;
 mod service;
 mod registry;
 
-use std::sync::Arc;
+use std::{sync::Arc, collections::HashMap};
 
 use actix_web::{App, HttpServer, web::ServiceConfig};
+use actix_web_prometheus::PrometheusMetricsBuilder;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     logger::init_logger()?;
     let registry = Arc::new(registry::Registry::new().await);
+    let mut labels = HashMap::new();
+    labels.insert("service".to_string(), "app".to_string());
+    let prometheus = PrometheusMetricsBuilder::new("api")
+        .endpoint("/internal/metrics")
+        .const_labels(labels)
+        .build()
+        .unwrap();
     HttpServer::new(move || {
         App::new()
+            .wrap(prometheus.clone())
             .configure(|cfg : &mut ServiceConfig| registry.configure(cfg))
             .configure(handler::configure)
     }).bind("0.0.0.0:8080")?
